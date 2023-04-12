bf20ad568d5e0c435f42a91693a1d53f002e270a
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-04-12 14:55:38 -0300

    ViewSets de Animes e Gêneros criadas.

--- animes/views.py
+++ animes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from animes.models import Genero, Anime
+from serializer import GeneroSerializer, AnimeSerializer
 
-# Create your views here.
+
+class AnimesViewSet(viewsets.ModelViewSet):
+    """Listando todos os animes."""
+    queryset = Anime.objects.all()
+    serializer_class = AnimeSerializer
+
+
+class GenerosViewSet(viewsets.ModelViewSet):
+    """Listando todos os gêneros."""
+    queryset = Genero.objects.all()
+    serializer_class = GeneroSerializer
