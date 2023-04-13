94fdf82bc3c8ab58249e8acc4951b5e5b320b830
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-04-13 11:34:04 -0300

    'BasicAuthentucation' e 'IsAuthenticated' adiconados aos ViewsStes.

--- animes/views.py
+++ animes/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, generics
 from animes.models import Genero, Anime
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 from animes.serializer import (
     GeneroSerializer,
     AnimeSerializer,
@@ -9,17 +11,30 @@ from animes.serializer import (
 
 class AnimesViewSet(viewsets.ModelViewSet):
     """Listando todos os animes."""
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializer
 
 
 class GenerosViewSet(viewsets.ModelViewSet):
     """Listando todos os gêneros."""
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Genero.objects.all()
     serializer_class = GeneroSerializer
 
 
 class ListaAnimesPorGenero(generics.ListAPIView):
+    """Listando animes por gêneros."""
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
         queryset = Anime.objects.filter(id=self.kwargs['pk'])
         return queryset
