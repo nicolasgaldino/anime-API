531e6b5a3768711b0cc66192b72a4cc88dd49705
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-04-18 19:37:29 -0300

    Carrosel adicionado. Ainda não está 100% do meu agrado, mas está funcional..

--- anime/js/home-page.js
+++ anime/js/home-page.js
@@ -12,6 +12,65 @@ fetch(url, {
 })
   .then(response => response.json())
   .then(data => {
+    const carouselData = data.slice(0, 5);
+
+    const carouselIndicators = document.querySelector('.carousel-indicators');
+    const carouselInner = document.querySelector('.carousel-inner');
+    carouselData.forEach((anime, index) => {
+      const active = index === 0 ? 'active' : '';
+      carouselIndicators.innerHTML += `<button type="button" data-bs-target="#animeCarousel" data-bs-slide-to="${index}" class="${active}" aria-current="${active}"></button>`;
+      carouselInner.innerHTML += `
+      <div class="carousel-item ${active}">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="image-container">
+              <img src="${anime.imagem}" class="d-block img-fluid" alt="${anime.nome}">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="d-flex flex-column justify-content-start align-items-start h-100">
+              <div class="fade-container rounded"></div>
+              <div class="text-container rounded">
+                <h5>${anime.nome}</h5>
+                <p>Gênero: ${anime.generos.map(genero => genero.nome).join(', ')}</p>
+                <p>Nota: ${anime.nota}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+
+    function setCarouselHeight() {
+      const carousel = document.querySelector('.carousel');
+      const carouselInner = document.querySelector('.carousel-inner');
+      const carouselItem = document.querySelector('.carousel-item');
+      const carouselImg = document.querySelector('.carousel-item img');
+
+      carousel.style.height = carouselImg.offsetHeight + 'px';
+
+      carouselInner.style.height = carouselImg.offsetHeight + 'px';
+
+      carouselItem.style.height = carouselImg.offsetHeight + 'px';
+    }
+
+    window.addEventListener('resize', setCarouselHeight);
+
+    window.dispatchEvent(new Event('resize'));
+    window.addEventListener('load', setCarouselHeight);
+  })
+  .catch(error => console.error('Erro ao carregar animes:', error));
+
+
+
+fetch(url, {
+  headers: {
+    'Authorization': authHeader
+  }
+})
+  .then(response => response.json())
+  .then(data => {
     const row = document.createElement('div');
     row.classList.add('row');
     animeList.appendChild(row);
