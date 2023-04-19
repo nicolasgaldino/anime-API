e13fbb1b90cf1dcbaf312c7a6da2d13d6150d30c
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-04-19 18:50:49 -0300

    Alteração quando a exibição de detalhes. Agora é aberto um modal, ao invés de levar a uma nova página.

--- anime/js/functions/createAnimeCard.js
+++ anime/js/functions/createAnimeCard.js
@@ -10,10 +10,50 @@ export function createAnimeCard(anime) {
       <button class="anime-card__button btn btn-success anime-card__button" data-anime-id="${anime.id}">Detalhes</button>
     </div>
   `;
-  animeCard.querySelector('.anime-card__button').addEventListener('click', (event) => {
-    event.preventDefault();
-    const animeId = anime.id;
-    window.location.href = `../../anime/details-page.html?id=${animeId}`;
-  });
+
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.classList.add('modal-oppened');
+  modal.innerHTML = `
+      <div class="container container-detail bg-light p-4 rounded">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="image-container">
+              <img src="${anime.imagem}" class="d-block img-fluid rounded" alt="${anime.nome}">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <h1 class="anime-text">${anime.nome}</h1>
+            <h2 class="anime-text text-truncate">${anime.nome_alternativo}</h2>
+            <p class="anime-text">Gênero: ${anime.generos.map(genero => genero.nome).join(', ')}</p>
+            <p class="anime-text">Estúdio: ${anime.estudio}</p>
+            <p class="anime-text">Diretor: ${anime.diretor}</p>
+            <p class="anime-text">Lançamento: ${moment(anime.data_lancamento).format('DD - MM - YYYY')}</p>
+            <p class="anime-text">Nota: ${anime.nota}</p>
+          </div>
+        </div>
+        <p class="sinopse">Sinopse: <br> ${anime.sinopse}</p>
+        <div class="d-flex justify-content-end mt-5">
+          <button class="close-button btn btn-secondary">Fechar</button>
+        </div>
+      </div>
+  `;
+
+  document.body.appendChild(modal);
+
+  if (animeCard.querySelector('.anime-card__button')) {
+    animeCard.querySelector('.anime-card__button').addEventListener('click', () => {
+      modal.style.display = 'block';
+      document.body.classList.add('modal-open');
+    });
+  }
+
+  if (modal.querySelector('.close-button')) {
+    modal.querySelector('.close-button').addEventListener('click', () => {
+      modal.style.display = 'none';
+      document.body.classList.remove('modal-open');
+    });
+  }
+
   return animeCard;
 }
