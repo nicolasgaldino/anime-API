9600f248c42373e513e86ca0713c4b9c34ce1a0c
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-05-10 14:26:51 -0300

    Botão para filtrar animes dublados ou legendados criado Preciso melhorar a ideia e a apresentação, mas por enquanto vou deixar assim.

--- anime/js/anime.js
+++ anime/js/anime.js
@@ -10,6 +10,7 @@ const PASSWORD = '22565721aA!';
 const AUTH_HEADER = 'Basic ' + btoa(`${USERNAME}:${PASSWORD}`);
 const PAGE_SIZE = 8;
 let currentPage = localStorage.getItem('currentPage') || 1;
+let filter = null;
 
 fetchAnimeData(CAROUSEL_INDICATORS, CAROUSEL_INNER, `${BASE_URL}`, AUTH_HEADER);
 
@@ -21,9 +22,15 @@ function updateCurrentPage(page) {
 
 // Por serem funções pequenas decidi mantê-las no JS principal
 
-// Função para carregar a página atual
+// ...
+
+// Função para carregar a página atual com o filtro de dublagem
 function loadCurrentPage() {
-  const url = `${BASE_URL}?page=${currentPage}`;
+  let url = `${BASE_URL}?page=${currentPage}`;
+
+  if (filter !== null) {
+    url += `&dublagem=${filter}`;
+  }
 
   fetch(url, {
     headers: {
@@ -40,6 +47,35 @@ function loadCurrentPage() {
     })
     .catch(error => console.error('Erro ao carregar animes:', error));
 }
+
+// Função para alternar entre dublados e legendados
+function toggleDublagem() {
+  const dublagemButton = document.getElementById('dublagem-button');
+  const isDublado = dublagemButton.classList.contains('active');
+
+  // Atualize a variável "filter" com o valor adequado
+  if (isDublado) {
+    dublagemButton.classList.remove('active');
+    dublagemButton.innerText = 'Legendado';
+    filter = false; // Legendado
+  } else {
+    dublagemButton.classList.add('active');
+    dublagemButton.innerText = 'Dublado';
+    filter = true; // Dublado
+  }
+
+  // Chame a função para carregar a página atual
+  loadCurrentPage();
+}
+
+// Função para limpar o filtro e exibir todos os animes
+function limparFiltro() {
+  filter = null
+  loadCurrentPage(); // Carrega a página atual sem filtro
+}
+
+// Adiciona o listener de clique ao botão de limpar filtro
+document.querySelector('#limpar-filtro').addEventListener('click', limparFiltro);
 
 // Função para avançar para a próxima página
 function nextPage() {
@@ -60,6 +96,9 @@ function previousPage() {
 // Adiciona os listeners de clique nos botões de paginação
 document.querySelector('#previous-page').addEventListener('click', previousPage);
 document.querySelector('#next-page').addEventListener('click', nextPage);
+
+// Adicione o listener de clique no botão de filtro
+document.getElementById('dublagem-button').addEventListener('click', toggleDublagem);
 
 // Carrega a página atual ao carregar a página
 loadCurrentPage();
